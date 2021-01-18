d777a863f24afa372afb94c0503efb87c05998a0
noaki-k <knk43nok@gmail.com>
2021-01-18 21:50:10 +0900

    chapter4

---
+++ chapter4/factory/product.go
@@ -0,0 +1,26 @@
+package chapter4
+
+/*
+product: 抽象メソッドuseのみ定義されている抽クラス
+factory: メソッドcreatを実装している抽象クラス
+IDCard: メソッドuseを実装している
+IdCardFactory: メソッドcreateProduct, registerProductを実装しているクラス
+ */
+
+type product interface {
+	use()
+}
+
+type factorize interface {
+	createProduct(owner string) string
+	registerProduct(p *product)
+}
+type factory struct {
+	factorize
+}
+
+func (f factory) NewProduct(owner string) *product {
+	p := f.createProduct(owner)
+	f.registerProduct(p)
+	return p
+}
